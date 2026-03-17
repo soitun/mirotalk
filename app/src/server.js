5ec6120e80290d275acd1372a6857162b353708a
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-03-17 02:00:57 +0100

    [mirotalk] - fix: preserve room parameter through login redirect when HOST_PROTECTED is enabled

--- app/src/server.js
+++ app/src/server.js
@@ -45,7 +45,7 @@ dependencies: {
  * @license For commercial use or closed source, contact us at license.mirotalk@gmail.com or purchase directly from CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-p2p-webrtc-realtime-video-conferences/38376661
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.7.44
+ * @version 1.7.45
  *
  */
 
@@ -683,7 +683,9 @@ app.get('/join/:roomId', function (req, res) {
     if (allowRoomAccess) {
         htmlInjector.injectHtml(views.client, res);
     } else {
-        !OIDC.enabled && hostCfg.protected ? res.redirect('/login') : res.redirect('/');
+        !OIDC.enabled && hostCfg.protected
+            ? res.redirect('/login?room=' + encodeURIComponent(roomId))
+            : res.redirect('/');
     }
 });
 
