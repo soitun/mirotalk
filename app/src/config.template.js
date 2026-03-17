538499ecc742647812dc75d71b1f62cc7717b471
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-03-17 03:03:10 +0100

    [mirotalk] - login page: flatten card structure and fix joinRoomForm background

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.7.48 - Configuration File
+ * MiroTalk P2P v.1.7.49 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
