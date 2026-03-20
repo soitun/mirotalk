90e423d16895060a095846c3162bcd7a42de6b16
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-03-20 03:18:34 +0100

    [mirotalk] - add missing footer in waiting room

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.7.56 - Configuration File
+ * MiroTalk P2P v.1.7.57 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
--- public/js/brand.js
+++ public/js/brand.js
@@ -107,7 +107,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.7.56',
+        title: 'WebRTC P2P v1.7.57',
         html: `
             <button 
                 id="support-button" 
