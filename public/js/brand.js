136cca13b816b51d8df15a5386b13ea4249d71d5
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-03-26 11:53:30 +0100

    [mirotalk] - fix -webkit-autofill

--- public/js/brand.js
+++ public/js/brand.js
@@ -107,7 +107,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.7.61',
+        title: 'WebRTC P2P v1.7.62',
         html: `
             <button 
                 id="support-button" 
