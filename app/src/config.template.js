61cbaae961c12d24cf430af61c37706daf53cc75
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-04-04 20:30:01 +0200

    [mirotalk] - align chat dropdown switch row icons with action button icons

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.7.93 - Configuration File
+ * MiroTalk P2P v.1.7.94 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
