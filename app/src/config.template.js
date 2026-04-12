e6ca71febca8259556ba03ed194dbd649720f2d1
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-04-13 01:58:41 +0200

    [mirotalk] - improve active rooms page UI/UX

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.8.00 - Configuration File
+ * MiroTalk P2P v.1.8.01 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
