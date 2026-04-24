0bd0ff7d622e8853792aaa133bed2216da6c9764
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-04-24 14:26:48 +0200

    [mirotalk] - add mirotalk linkedin

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk P2P v.1.8.10 - Configuration File
+ * MiroTalk P2P v.1.8.11 - Configuration File
  * ==============================================
  *
  * This file is the central configuration source.
