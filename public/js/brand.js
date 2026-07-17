6e952f5b5c3a07b42522599b99b0ccf8646964c0
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-07-17 11:11:17 +0200

    [mirotalk] #340 - fix(client): fix invisible device dropdown options (white-on-white) and update dep

--- public/js/brand.js
+++ public/js/brand.js
@@ -109,7 +109,7 @@ let brand = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: 'WebRTC P2P v1.8.67',
+        title: 'WebRTC P2P v1.8.68',
         html: `
             <button 
                 id="support-button" 
